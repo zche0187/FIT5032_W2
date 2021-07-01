031976f5a5b6e551b6441e14ae353ed7876eef79
zche0187 <zche0187@student.monash.edu>
2021-07-01 21:54:07 +0800

    first commit

---
+++ FIT5032_MyCodeSnippet/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using FIT5032_MyCodeSnippet.Models.HelloWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_MyCodeSnippet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+            
+            Hello hello = new Hello();
+            
+            ViewBag.Message = hello.GetHello();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
